53877e41e295b32fb28c44dad7f6d45459f2b83b
Pascual Camacho <pascualc02@gmail.com>
2023-12-21 14:22:53 -0600

    added config and python files

---
+++ chatapp/state.py
@@ -0,0 +1,44 @@
+# state.py
+import reflex as rx
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key='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')
+
+
+
+
+class State(rx.State):
+    # The current question being asked.
+    question: str
+
+    # Keep track of the chat history as a list of (question, answer) tuples.
+    chat_history: list[tuple[str, str]]
+
+    def answer(self):
+        # Our chatbot has some brains now!
+        session = client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": self.question}
+        ],
+        stop=None,
+        temperature=0.7,
+        stream=True)
+
+        # Add to the answer as the chatbot responds.
+        answer = ""
+        self.chat_history.append((self.question, answer))
+
+        # Clear the question input.
+        self.question = ""
+        # Yield here to clear the frontend input before continuing.
+        yield
+
+        for item in session:
+            if hasattr(item.choices[0].delta, "content"):
+                answer += item.choices[0].delta.content
+                self.chat_history[-1] = (
+                    self.chat_history[-1][0],
+                    answer,
+                )
+                yield
